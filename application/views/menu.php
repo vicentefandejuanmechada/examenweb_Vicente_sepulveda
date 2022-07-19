2bab66cb4dc48ab5c966969cab2fb8dd826f76d5
Pedro Garrido <92741841+pedro-garrido@users.noreply.github.com>
2022-07-19 02:25:33 -0400

    a

---
+++ application/views/menu.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="contenedor">
+    <header class="superior">
+        <div class="logo">
+          <a href="<?php echo base_url(); ?>"> <img src="<?php echo base_url('artilugioslacarpeta/img/logo.png');?>" alt="Logo" ></a>
+        </div>
+        <nav class="menu nav-bar"></nav>
+        <ul>
+            <li><a href='base_url'>Inicio</a></li>
+</ul>
+        </header>
+</div>
