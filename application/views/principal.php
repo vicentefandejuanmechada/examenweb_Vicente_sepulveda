16cb07102269eb41b6ce2d6e7e4abbbb227d37f0
vicentefandejuanmechada <93413831+vicentefandejuanmechada@users.noreply.github.com>
2022-07-19 03:14:32 -0400

    listo

--- application/views/principal.php
+++ application/views/principal.php
@@ -7,12 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taller Don gualo</title>
-    <script src="<?php echo base_url(); ?>artilugioslacarpeta/js/bootstrap.min.js"></script>  
-    <script src="<?php echo base_url(); ?>artilugioslacarpeta/js/sweetalert2.all.min.js"></script>
-    <script src="<?php echo base_url(); ?>artilugioslacarpeta/js/script.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('artilugioslacarpeta/css/bootstrap.min.css'); ?>" />
+  <link rel="stylesheet" href="<?php echo base_url('artilugioslacarpeta/css/style.css'); ?>" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>  
+  <script src="<?php echo base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/script.js"></script>
+   
 </head>
 <body>
-<?php $this->load->view('navbar'); ?>
+
 <section>
     <img src="<?php echo base_url('artilugioslacarpeta/img/principal.jpg');?>" alt="" id="imgprincipal" >
 </section>    
@@ -52,7 +56,7 @@
         </section>
         <footer class="pie">
             <div class="numero">
-                <img src="img/logo.png" alt="imagen logo footer" />
+                <img src="img/logo.png" alt="" />
                 <p>Estamos Ubicados en Chanquico Sur S/N - San Javier</p>
                 <p>Llámanos al +569 12312345</p>
             <br>
